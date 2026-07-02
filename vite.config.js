c6eeb5c97f6d15aa593fac8448c87bcce2b4901a
melvzz1204 <137977788+melvzz1204@users.noreply.github.com>
2026-07-02 16:20:08 +0800

    new

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        sample: resolve(__dirname, "sample.html"),
+        home: resolve(__dirname, "home.html"),
+      },
+    },
+  },
+});
